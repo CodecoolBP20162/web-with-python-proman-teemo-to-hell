99ee26b097a719803554585137d5383364a45d09
Péter Vetési <vetesipeti@gmail.com>
2017-03-14 13:49:36 +0100

    javascript in progress

--- static/java.js
+++ static/java.js
@@ -1,3 +1,25 @@
-/**
- * Created by bezi on 2017.03.13..
- */
+$(document).ready(function() {
+    localStorage.boardStorage = {};
+    function Board(name) {
+        this.name = name;
+    }
+    var newcard = "<li class='card'>" +
+        "<div class='title-bar'>" +
+        "<div class='card-title'></div>" +
+        "</div>" +
+        "</li>";
+    for (var key in localStorage) {
+        var card = "<li class='card'>" +
+        "<div class='title-bar'>" +
+        "<div class='card-title'>" + localStorage.getItem("name1") + "</div>" +
+        "</div>" +
+        "</li>";
+        document.getElementById("grid-wrapper").innerHTML = card;
+    }
+
+    $(".card").click(function(){
+        $("#grid-wrapper").append(newcard);
+        localStorage.setItem("name1", "webstore1");
+    })
+
+})
