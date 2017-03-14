7ea3765766cf7f3345103f3e86491af3c5511a3c
kalcifield <bezilaszlo95@gmail.com>
2017-03-14 14:10:28 +0100

    bugfix

--- static/java.js
+++ static/java.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     localStorage.boardStorage = {};
     function Board(name) {
         this.name = name;
     }
+
     var newcard = "<li class='card'>" +
         "<div class='title-bar'>" +
         "<div class='card-title'></div>" +
@@ -10,16 +11,38 @@ $(document).ready(function() {
         "</li>";
     for (var key in localStorage) {
         var card = "<li class='card'>" +
-        "<div class='title-bar'>" +
-        "<div class='card-title'>" + localStorage.getItem("name1") + "</div>" +
-        "</div>" +
-        "</li>";
+            "<div class='title-bar'>" +
+            "<div class='card-title'>" + localStorage.getItem("name1") + "</div>" +
+            "</div>" +
+            "</li>";
         document.getElementById("grid-wrapper").innerHTML = card;
     }
 
-    $(".card").click(function(){
+    $("#save-button").click(function () {
         $("#grid-wrapper").append(newcard);
         localStorage.setItem("name1", "webstore1");
     })
+
+    $('.pull-me').click(function () {
+        $('.panel').slideToggle(200);
+    });
+
+
+    var loading = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.target.classList.add('loading');
+        e.target.setAttribute('disabled', 'disabled');
+        setTimeout(function () {
+            e.target.classList.remove('loading');
+            e.target.removeAttribute('disabled');
+        }, 500);
+    };
+
+    var btns = document.querySelectorAll('button');
+    for (var i = btns.length - 1; i >= 0; i--) {
+        btns[i].addEventListener('click', loading);
+    }
+    ;
 
 })
