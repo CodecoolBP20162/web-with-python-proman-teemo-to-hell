16adc9ab4b6134c8db1ea61b55b9693fdad59a2d
Péter Vetési <vetesipeti@gmail.com>
2017-03-30 11:18:13 +0200

    flask: empty lines are deleted, card related styles added

--- flask_app.py
+++ flask_app.py
@@ -4,11 +4,10 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-
-
 
 
 if __name__ == '__main__':
